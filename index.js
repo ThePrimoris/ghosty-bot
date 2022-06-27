f84deea94f432a1cd429fd440d942ecbdedc7876
Primoris <30489975+ThePrimoris@users.noreply.github.com>
2022-06-27 09:35:38 -0500

    made github repository

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.login(process.env.TOKEN)
